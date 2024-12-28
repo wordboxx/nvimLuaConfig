07e0949fb863c8967a4dea398001ab1ace7f3cd4
wordboxx <79267124+wordboxx@users.noreply.github.com>
2024-12-28 10:45:10 +0000

    init commit

---
+++ config/options.lua
@@ -0,0 +1,12 @@
+-- Options are automatically loaded before lazy.nvim startup
+-- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
+-- Add any additional options here
+--
+-- Tabs/Spacing
+vim.o.tabstop = 4 -- A TAB character looks like 4 spaces
+vim.o.expandtab = false -- Pressing the TAB key will insert spaces instead of a TAB character
+vim.o.softtabstop = 4 -- Number of spaces inserted instead of a TAB character
+vim.o.shiftwidth = 4 -- Number of spaces inserted when indenting
+
+-- Always open CWD for file in Terminal
+vim.opt.autochdir = true
